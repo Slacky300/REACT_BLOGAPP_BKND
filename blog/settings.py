66811227ed60b256d8a3f87339aa484ebfbc06b6
Rehman Khan <rehman.khan.300a@gmail.com>
2023-05-25 17:07:21 +0530

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -81,8 +81,15 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EventManagementS$blog',
+        'USER': 'EventManagementS',
+        'PASSWORD': 'RehmanKhan123DB',
+        'HOST':'EventManagementSystem.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
@@ -168,10 +175,19 @@ AUTH_USER_MODEL = 'blogApp.UserAccount'
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOWED_ORIGINS = [
+
+    "http://localhost:3000",
+]
+
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:3000",
+]
 
