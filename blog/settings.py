c30f5aea9a79ad449a8dcf695d68cc8c63140da0
Rehman Khan <rehman.khan.300a@gmail.com>
2023-06-09 22:47:26 +0530

    updated database settings

--- blog/settings.py
+++ blog/settings.py
@@ -84,21 +84,21 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'EventManagementS$blog',
-    #     'USER': 'EventManagementS',
-    #     'PASSWORD': 'RehmanKhan123DB',
-    #     'HOST':'EventManagementSystem.mysql.pythonanywhere-services.com',
-    #     'PORT':'3306',
-    #     'OPTIONS': {
-    #         'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-    #     }
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'EventManagementS$blog',
+        'USER': 'EventManagementS',
+        'PASSWORD': 'RehmanKhan123DB',
+        'HOST':'EventManagementSystem.mysql.pythonanywhere-services.com',
+        'PORT':'3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
 }
 
 
